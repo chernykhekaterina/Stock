b6e8916402f70746474c0a396383620ee0c19a79
Ekaterina <ekaterina@MacBook-Air-Ekaterina.local>
2019-04-02 18:01:25 +0100

    new

--- Project.py
+++ Project.py
@@ -1,3 +1,8 @@
+def _iter_loaders(self, template):
+        loader = self.app.jinja_loader
+        if loader is not None:
+            yield self.app, loader
+
 from flask import Flask, render_template, request
 
 import pandas as pd
@@ -6,10 +11,8 @@ from alpha_vantage.timeseries import TimeSeries
 import matplotlib.pyplot as plt
 import sys
 
-def _iter_loaders(self, template):
-        loader = self.app.jinja_loader
-        if loader is not None:
-            yield self.app, loader
+
+
 app = Flask("Final_Project")
 app.debug = True
 
@@ -17,29 +20,25 @@ app.debug = True
 def stockchart():
 
     print "inside stockchart"
-    select = request.form.get("symbol")
-    print select
+    symbol = request.form.get("symbol")
+    print symbol
 
     ts = TimeSeries(key='your_key', output_format='pandas')
     data, meta_data = ts.get_intraday(symbol=symbol,interval='1day', outputsize='full')
     data['close'].plot()
     plt.title('1 day performance of {} stock').format(symbol)
     plt.show()
-
+    print data
+    print meta_datae
     symbol=input("Pick a Tech Stock of Your interest:")
     stockchart(symbol)
-    return "selected Symbol" + select
+    return "selected Symbol" + symbol
 
 @app.route("/")
 def dropdown():
     symbol_list = ["GOOGL","MSFT","IBM", "AAPL"]
-    return render_template("index.html", symbol_list="symbol_list")
+    return render_template("index.html", symbol_list=symbol_list)
 
 
-
-
-#change raw input to smth else so that choice is in html rather than in terminal
 if __name__ == "__main__":
     app.run()
-
-#Please enter sector of interest to get S&P performance - limit it to a list that define in html
